984b702ad0a861d0048816760ea3a42483c0d184
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-14 07:22:52 +0300

    feat: Initialize Song class with instance attributes and update class-level tracking

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,20 @@
 class Song:
-    pass
+    #class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count  =[]
+    artist_count = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    #update class-level data
+    self.add_song_to_count()
+    self.add_song_to_genres(genre)
+    self.add_to_artists(artist)
+    self.add_to_genre_count(genre)
+    self.add_to_artist_count(artist)
+
