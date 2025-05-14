0af851e32cc0c2b614a67f2cf2426fb4133f5631
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-14 07:35:34 +0300

    fix: Move instance initialization inside __init__ and correct data types for genre/artist count

--- lib/song.py
+++ lib/song.py
@@ -1,22 +1,22 @@
 class Song:
-    #class attributes
+    # Class attributes
     count = 0
     genres = []
     artists = []
-    genre_count  =[]
-    artist_count = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
 
-    #update class-level data
-    self.add_song_to_count()
-    self.add_song_to_genres(genre)
-    self.add_to_artists(artist)
-    self.add_to_genre_count(genre)
-    self.add_to_artist_count(artist)
+        # Update class-level data
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
@@ -37,7 +37,7 @@ class Song:
         if genre in cls.genre_count:
             cls.genre_count[genre] += 1
         else:
-            cls.genre_count[genre] += 1
+            cls.genre_count[genre] = 1
 
     @classmethod
     def add_to_artist_count(cls, artist):
